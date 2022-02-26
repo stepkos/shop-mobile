38ff091c157a80a84e5e4a6bd935a3a2f6c86c5c
stepkos <stepkos@wp.pl>
2022-02-26 23:12:15 +0100

    New initial products

--- app/src/main/java/com/example/shopapp/ProductModel.java
+++ app/src/main/java/com/example/shopapp/ProductModel.java
@@ -30,9 +30,11 @@ public class ProductModel {
 
     public static ArrayList<ContentValues> getInitialData() {
         return new ArrayList<>(Arrays.asList(
-                getCVFromData(R.drawable.set1, "Komputer 1", "opis 1", 3999),
-                getCVFromData(R.drawable.set2, "Komputer 2", "opis 2", 7999),
-                getCVFromData(R.drawable.set3, "Komputer 3", "opis 3", 11999)
+                getCVFromData(R.drawable.set1, "Komputer Game X G100", "Core i5-10400F, 16 GB, GTX 1060, 512 GB M.2 PCIe", 3699),
+                getCVFromData(R.drawable.set2, "Komputer Game X G300", "Core i5-10400F, 16 GB, RTX 3060, 1 TB M.2 PCIe", 5349),
+                getCVFromData(R.drawable.set3, "Komputer Game X G700", "Core i9-10850K, 16 GB, RTX 3080 Ti, 1 TB M.2 PCIe", 15499),
+                getCVFromData(R.drawable.keyboard1, "Klawiatura SPC Gear GK630K", "Przelaczniki mechaniczne - Kailh Blue, Podswietlenie RGB", 279),
+                getCVFromData(R.drawable.mouse1, "Mysz SteelSeries Rival 600", "12000 DPI, 7 Przyciskow, TrueMove3+ Dual Sensor System", 349)
         ));
     }
 
